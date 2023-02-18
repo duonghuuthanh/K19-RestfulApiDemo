6c88ec0f32560dcc850889a1bef2c19339e0193a
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2023-02-18 20:00:08 +0700

    create project c4

---
+++ ecourseappv4/ecourseapp/courses/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Category, Course
+
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class CourseForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'subject', 'created_date', 'active']
+    search_fields = ['subject']
+    list_filter = ['id', 'subject', 'created_date']
+    form = CourseForm
+
+
+admin.site.register(Category)
+admin.site.register(Course, CourseAdmin)
---
+++ ecourseappv4/ecourseapp/courses/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from ckeditor.fields import RichTextField
+
+
+class User(AbstractUser):
+    pass
+
+
+class BaseModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(BaseModel):
+    subject = models.CharField(max_length=255)
+    description = RichTextField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    image = models.ImageField(upload_to='courses/%Y/%m', null=True)
+
+    def __str__(self):
+        return self.subject
