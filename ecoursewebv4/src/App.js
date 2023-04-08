f3a30a8a563d411ba89e34965b7adc0ac035c9e0
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2023-04-08 19:24:51 +0700

    add react context v4

--- ecoursewebv4/src/App.js
+++ ecoursewebv4/src/App.js
@@ -1,28 +1,38 @@
+import { useReducer } from 'react';
 import { Container } from 'react-bootstrap';
+import cookie from 'react-cookies';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Courses from './components/Courses';
 import LessonDetails from './components/LessonDetails';
 import Lessons from './components/Lessons';
+import Login from './components/Login';
+import { MyUserContext } from './configs/MyContext';
 import Footer from './layouts/Footer';
 import Header from './layouts/Header';
+import myUserReducer from './reducers/MyUserReducer';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      
-      <Container>
-        <Routes>
-          <Route path='/' element={<Courses /> } />
-          <Route path='/courses/:courseId/lessons' element={<Lessons />} />
-          <Route path='/lessons/:lessonId' element={<LessonDetails />} />
-          <Route path='*' element={<div className='alert alert-info m-1'>Comming soon...</div>} />
-        </Routes>
-      </Container>
+  const [user, dispatch] = useReducer(myUserReducer, cookie.load('current-user') || null)
 
-      <Footer />
-    </BrowserRouter> 
+  return (
+    <MyUserContext.Provider value={[user, dispatch]}>
+      <BrowserRouter>
+        <Header />
+        
+        <Container>
+          <Routes>
+            <Route path='/' element={<Courses /> } />
+            <Route path='/courses/:courseId/lessons' element={<Lessons />} />
+            <Route path='/lessons/:lessonId' element={<LessonDetails />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='*' element={<div className='alert alert-info m-1'>Comming soon...</div>} />
+          </Routes>
+        </Container>
+
+        <Footer />
+      </BrowserRouter> 
+    </MyUserContext.Provider>
   );
 }
 
--- ecoursewebv4/src/layouts/Header.js
+++ ecoursewebv4/src/layouts/Header.js
@@ -1,12 +1,14 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Button, Container, Form, Nav, Navbar } from "react-bootstrap"
 import { Link, useNavigate } from "react-router-dom"
 import API, { endpoints } from "../configs/API"
+import { MyUserContext } from "../configs/MyContext"
 
 const Header = () => {
     const [categories, setCategories] = useState([])
     const [q, setQ] = useState("")
     const nav = useNavigate()
+    const [user, dispatch] = useContext(MyUserContext)
 
     useEffect(() => {
         const loadCategories = async () => {
@@ -22,21 +24,45 @@ const Header = () => {
         nav(`/?q=${q}`)
     }
 
+    const logout = () => {
+        dispatch({
+            "type": "logout"
+        })
+    }
+
+    let userInfo = (
+        <>
+            <Link className="nav-link text-danger" to="/login">&#128104; Đăng nhập</Link>
+        </>
+    )
+
+    if (user !== null)
+        userInfo = (
+            <>
+                <Link className="nav-link text-danger" to="/">
+                    <img src={user.image} alt={user.username} width="30" className="rounded-circle" />
+                    Chào {user.username}
+                </Link>
+                <Button className="btn btn-danger" onClick={logout}>&#128104; Đăng xuất</Button>
+            </>
+        )
+
     return (
         <>
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">&#9997; e-Course Web</Navbar.Brand>
+                <Link to="/" className="navbar-brand">&#9997; e-Course Web</Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Link className="nav-link" to="/">Trang chủ</Link>
+                    <Link className="nav-link active" to="/">&#127968; Trang chủ</Link>
                     {categories.map(c => {
                         let url = `/?cateId=${c.id}`
 
                         return <Link className="nav-link" to={url} key={c.id}>{c.name}</Link>
                     })}
-                   
+                    
+                    {userInfo}
                 </Nav>
                 <Form onSubmit={search} className="d-flex">
                     <Form.Control type="search" placeholder="Tên khóa học..."
